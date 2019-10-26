9f80dcd21d8818b7fdbd571950849a91f616d892
Sebastian Arango <sjarango92@gmail.com>
2019-10-26 16:10:16 -0400

    MySQL file creating database

---
+++ bamazon_db.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(30) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Casio Watch", "watch", 14.95, 30), ("Rolex Watch", "watch", 8550.00, 3), ("Apple Watch", "watch", 399.00, 50), 
+("Timex Watch", "watch", 10.00, 40), ("iPhone 11", "phone", 699.00, 500), ("iPhone 11 Pro", "phone", 999.00, 500), 
+("Google Pixel 3", "phone", 499.00, 400), ("Google Pixel 4", "phone", 799.00, 500), ("Samsung Galaxy s10", "phone", 899.00, 500);
+
+SELECT * FROM bamazon.products;
