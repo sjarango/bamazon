7a65b0fc132e83da82aca7236dd5264c18550ff8
Sebastian Arango <sjarango92@gmail.com>
2019-10-29 13:22:35 -0400

    Query used to retrieve price and stock, values used for basic logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,55 +28,73 @@ function queryAllProducts() {
         console.log("\n-----------------------------------------");
         console.log("Item ID -- Product Name -- Price ($)\n");
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + "  --  " + res[i].product_name + "  --  " + res[i].price );
+            console.log(res[i].item_id + "  --  " + res[i].product_name + "  --  " + res[i].price);
         }
         console.log("-----------------------------------------");
-        connection.end();
+        //connection.end();
         start();
     });
 }
 
-let itemID = null;
-let itemAmt = null;
+let inputID = null;
+let inputAmt = null;
 
-function start(){
+function start() {
+
+    let itemPrice = null;
+    let itemStock = null;
+
     inquirer
         .prompt({
             name: "id_num",
             type: "number",
             message: "Enter the Item ID number for the item you'd like to purchase."
         })
-        .then(function(answer) {
-            if (answer.id_num < 1 || answer.id_num > 10){
+        .then(function (answer) {
+            if (answer.id_num < 1 || answer.id_num > 10) {
                 console.log("Please enter correct an ID number between 1 and 10");
                 start();
             } else {
                 console.log("inputed ID number: " + answer.id_num);
-                itemID = answer.id_num;
-                amountRequested();
+                inputID = answer.id_num;
+                connection.query("SELECT price, stock_quantity FROM products WHERE item_id = ?",
+                    [inputID],
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log(res);
+                        itemPrice = parseFloat(res[0].price);
+                        itemStock = parseFloat(res[0].stock_quantity);
+                        console.log("price: " + itemPrice);
+                        console.log("stock: " + itemStock);
+                        connection.end();
+                        amountRequested(itemStock);
+                    });
             }
         });
 }
 
-function amountRequested(){
+function amountRequested(stock) {
     inquirer
         .prompt({
             name: "amount",
             type: "number",
             message: "How many units of the product would you like to buy?"
         })
-        .then(function(answer) {
-            if(answer.amount < 1){
+        .then(function (answer) {
+
+            if (answer.amount < 1) {
                 console.log("Please enter a value larger than 0.");
                 amountRequested();
+            } else if (answer.amount > stock) {
+                console.log("Insufficient Quantity!");
             } else {
                 console.log("inputed amount: " + answer.amount);
-                itemAmt = answer.amount;
-                receipt(itemID, itemAmt);
+                inputAmt = answer.amount;
+                receipt(inputID, inputAmt);
             }
         });
 }
 
-function receipt(id, amt){
+function receipt(id, amt) {
     console.log("id " + id + " amt: " + amt);
 }
